02774f214263e0479bb83a2e32ce5f677944f120
HuYssss <lehuyburn23@gmail.com>
2024-12-02 06:53:36 +0700

    add server sent event for get all order

--- watches-store-v2/src/main/java/hcmute/edu/vn/watches_store_v2/controller/admin/ManageOrderController.java
+++ watches-store-v2/src/main/java/hcmute/edu/vn/watches_store_v2/controller/admin/ManageOrderController.java
@@ -1,18 +1,24 @@
 package hcmute.edu.vn.watches_store_v2.controller.admin;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hcmute.edu.vn.watches_store_v2.base.ControllerBase;
 import hcmute.edu.vn.watches_store_v2.dto.order.request.CancelOrder;
 import hcmute.edu.vn.watches_store_v2.dto.order.response.OrderResponse;
+import hcmute.edu.vn.watches_store_v2.mapper.OrderMapper;
 import hcmute.edu.vn.watches_store_v2.service.business.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -68,6 +74,34 @@ public class ManageOrderController extends ControllerBase {
                 return response(null, HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             return response(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_ACCESS_FULL_SYSTEM')")
+    @GetMapping(value = "/get-all-order-realtime", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getAllRealtimeOrder() {
+//        Order order = this.orderRepository.findById(new ObjectId("673a021b8099935668157de1")).orElse(null);
+
+//        List<OrderResponse> resp = this.orderRepository.findAll().stream().map(OrderMapper::mapOrderResp).collect(Collectors.toList());
+
+        List<OrderResponse> resp = this.orderService.getAllOrders();
+
+        return Flux.interval(Duration.ofSeconds(5))
+                .map(sequence -> {
+                    String data = toJson(resp);
+                    log.info("Send data: {}", data);
+                    return data;
+                })
+                .doOnCancel(() -> log.info("Disconnection"));
+    }
+
+    private String toJson(Object object) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting to JSON", e);
         }
     }
 }
--- watches-store-v2/src/main/java/hcmute/edu/vn/watches_store_v2/controller/test/TestController.java
+++ watches-store-v2/src/main/java/hcmute/edu/vn/watches_store_v2/controller/test/TestController.java
@@ -1,9 +1,12 @@
 package hcmute.edu.vn.watches_store_v2.controller.test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hcmute.edu.vn.watches_store_v2.base.ControllerBase;
+import hcmute.edu.vn.watches_store_v2.dto.order.response.OrderResponse;
 import hcmute.edu.vn.watches_store_v2.dto.product.Option;
 import hcmute.edu.vn.watches_store_v2.entity.Order;
 import hcmute.edu.vn.watches_store_v2.entity.Product;
+import hcmute.edu.vn.watches_store_v2.mapper.OrderMapper;
 import hcmute.edu.vn.watches_store_v2.repository.OrderRepository;
 import hcmute.edu.vn.watches_store_v2.repository.ProductRepository;
 import hcmute.edu.vn.watches_store_v2.service.business.OrderService;
@@ -12,13 +15,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/test")
@@ -27,6 +34,7 @@ import java.util.List;
 public class TestController extends ControllerBase {
 
     private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
 
     @GetMapping("/update")
     public ResponseEntity<String> update() {
@@ -39,5 +47,30 @@ public class TestController extends ControllerBase {
         }
         productRepository.saveAll(products);
         return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/get-all-order-realtime", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getAllRealtimeOrder() {
+//        Order order = this.orderRepository.findById(new ObjectId("673a021b8099935668157de1")).orElse(null);
+
+        List<OrderResponse> resp = this.orderRepository.findAll().stream().map(OrderMapper::mapOrderResp).collect(Collectors.toList());
+
+        return Flux.interval(Duration.ofSeconds(5))
+                .map(sequence -> {
+                    String data = toJson(resp);
+                    log.info("Send data: {}", data);
+                    return data;
+                })
+                .doOnCancel(() -> log.info("Disconnection"));
+    }
+
+    private String toJson(Object object) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting to JSON", e);
+        }
     }
 }
