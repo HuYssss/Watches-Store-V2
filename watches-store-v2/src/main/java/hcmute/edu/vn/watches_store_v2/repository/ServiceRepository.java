de1d76f572fa360174553ecfbf3c7d9d6c9fb8af
HuYssss <lehuyburn23@gmail.com>
2024-12-12 10:40:08 +0700

    commit at 12/12/2024

--- watches-store-v2/src/main/java/hcmute/edu/vn/watches_store_v2/repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package hcmute.edu.vn.watches_store_v2.repository;
-
-import hcmute.edu.vn.watches_store_v2.entity.Service;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ServiceRepository extends MongoRepository<Service, ObjectId> {
-}
